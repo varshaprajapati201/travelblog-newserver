8eb6cafb3f23e0d4882e749bc55b6c8790868833
varshaprajapati201 <108179023+varshaprajapati201@users.noreply.github.com>
2022-09-15 23:24:43 +0530

    Add files via upload

---
+++ routes/article-route.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const {addArticle, getArticle, deleteArticle,getArticleById, getArticleToEdit, editArticle} = require("../controller/article-controller.js");
+
+const upload = require("../utils/upload.js")
+
+
+const ArticleRouter = express.Router();
+
+
+ArticleRouter.post("/Compose",addArticle)
+ArticleRouter.get("/all",getArticle);
+ArticleRouter.delete("/:id",deleteArticle);
+ArticleRouter.get("/detailView/:id",getArticleById);
+ArticleRouter.get("/:id",getArticleToEdit);
+ArticleRouter.post("/:id",editArticle);
+
+module.exports = ArticleRouter;
